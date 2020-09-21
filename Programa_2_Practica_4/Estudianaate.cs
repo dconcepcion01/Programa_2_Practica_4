df59889b9dbfb40b8a453a47da957f5d83778ccf
Danny <dannyconcepcion01@gmail.com>
2020-09-20 20:15:49 -0400

    Agregar archivos de proyecto.

---
+++ Programa_2_Practica_4/Estudianaate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_2_Practica_4
+{
+    public  class Estudianaate
+
+    {
+        private int matricula;
+        private string nombre;
+        private string apellido;
+        private string telefono;
+        private string correo;
+        private int promedio;
+        private string direccion;
+        private string seccion;
+
+        public void setmatricula(int matricula)
+        {
+            this.setmatricula = matricula;
+        }
+        public string setmatricula()
+        {
+            return this.matricula;
+        }
+
+        public void setnombre(string vnombre)
+        {
+            this.nombre = vnombre;
+        }
+        public string getnombre()
+
+        {
+        return this.nombres;
+        }
+
+
+
+        
+ 
+        
+    }
+    
+
+   
+}
